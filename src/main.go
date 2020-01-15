d30cf4f4e30b6b840078aaba697f549c716c9f1a
Jhorlin De Armas <jhorlin@gmail.com>
2020-01-15 14:02:09 -0500

    added error loging

--- src/main.go
+++ src/main.go
@@ -13,6 +13,7 @@ import (
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/s3"
+	log "github.com/sirupsen/logrus"
 )
 
 type BuildDetail struct {
@@ -20,16 +21,22 @@ type BuildDetail struct {
 	Name   string `json:"project-name"`
 }
 
+func init() {
+	log.SetReportCaller(true)
+}
+
 //HandleRequest handle she change for a given codebuild alarm
 func HandleRequest(ctx context.Context, event events.CloudWatchEvent) error {
 	buildDetail := BuildDetail{}
 	if err := json.Unmarshal(event.Detail, &buildDetail); err != nil {
+		log.Errorf("Unable to unmarshal event details %v", err)
 		return fmt.Errorf("Unable to unmarshal event details %w", err)
 	}
 
 	bucket := os.Getenv("BUCKET")
 
 	if len(bucket) <= 0 {
+		log.Errorf("Unable to update project %s with a status of %s, Please set BUCKET environment variable", buildDetail.Name, buildDetail.Status)
 		return fmt.Errorf("Unable to update project %s with a status of %s, Please set BUCKET environment variable", buildDetail.Name, buildDetail.Status)
 	}
 
@@ -39,6 +46,7 @@ func HandleRequest(ctx context.Context, event events.CloudWatchEvent) error {
 	sess, sessErr := session.NewSession()
 
 	if sessErr != nil {
+		log.Errorf("Unable to create a session %v", sessErr)
 		return fmt.Errorf("Unable to create a session %w", sessErr)
 	}
 
@@ -47,12 +55,14 @@ func HandleRequest(ctx context.Context, event events.CloudWatchEvent) error {
 	// copy the object
 	_, copyErr := svc.CopyObject(&s3.CopyObjectInput{Bucket: aws.String(bucket), CopySource: aws.String(source), Key: aws.String(destination)})
 	if copyErr != nil {
+		fmt.Errorf("Error copying status from %s to %s %v", source, destination, copyErr)
 		return fmt.Errorf("Error copying status from %s to %s %w", source, destination, copyErr)
 	}
 
 	// Wait to see if the item got copied
 	copyWaitErr := svc.WaitUntilObjectExists(&s3.HeadObjectInput{Bucket: aws.String(source), Key: aws.String(destination)})
 	if copyWaitErr != nil {
+		log.Errorf("Error waiting for item %s to be copied to %s %v", source, destination, copyWaitErr)
 		return fmt.Errorf("Error waiting for item %s to be copied to %s %w", source, destination, copyWaitErr)
 	}
 
